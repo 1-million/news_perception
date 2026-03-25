3c671ec1661fb33e773d2c5a536c94ab872bb217
moilion <593747881@qq.com>
2026-03-25 19:08:41 +0800

    v2版本-用最简单方式实现了会话记忆。

--- src/main/java/com/qr/np/controller/ChatController.java
+++ src/main/java/com/qr/np/controller/ChatController.java
@@ -1,17 +1,25 @@
 package com.qr.np.controller;
 
 import com.qr.np.dto.ChatRequest;
-import com.qr.np.dto.ChatResponse;
+import com.qr.np.dto.ResultResponse;
+import dev.langchain4j.data.message.ChatMessage;
+import dev.langchain4j.data.message.UserMessage;
 import dev.langchain4j.model.chat.ChatModel;
+import dev.langchain4j.model.chat.response.ChatResponse;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class ChatController {
 
     @Resource
     private ChatModel chatModel;
+
+    private final List<ChatMessage> memorys = new ArrayList<>();
 
     @GetMapping("/")
     public String index() {
@@ -20,8 +28,10 @@ public class ChatController {
 
     @PostMapping("/api/chat")
     @ResponseBody
-    public ChatResponse chat(@RequestBody ChatRequest request) {
-        String response = chatModel.chat(request.getMessage());
-        return new ChatResponse(response, true);
+    public ResultResponse chat(@RequestBody ChatRequest request) {
+        memorys.add(UserMessage.userMessage(request.getMessage()));
+        ChatResponse response = chatModel.chat(memorys);
+        memorys.add(UserMessage.userMessage(response.aiMessage().text()));
+        return new ResultResponse(response.aiMessage().text(), true);
     }
 }
--- src/main/java/com/qr/np/dto/ResultResponse.java
+++ src/main/java/com/qr/np/dto/ResultResponse.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class ChatResponse {
+public class ResultResponse {
     private String response;
     private boolean success;
 }
